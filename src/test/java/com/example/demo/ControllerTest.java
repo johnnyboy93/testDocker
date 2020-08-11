a8600dc921f35d9ed1ae2666a88c7c85bb56af80
Hassan <apzhawk@gmail.com>
2020-08-11 15:13:11 +0800

    4th

--- src/test/java/com/example/demo/ControllerTest.java
+++ src/test/java/com/example/demo/ControllerTest.java
@@ -9,7 +9,6 @@ import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -34,7 +33,6 @@ class ControllerTest {
 	
 	@Test
 	public void test() throws Exception {
-		System.out.println(mockmvc);
 		mockmvc.perform(MockMvcRequestBuilders.get("/hi")).andExpect(MockMvcResultMatchers.status().isOk())
 		.andExpect(MockMvcResultMatchers.content().string("Hello World"));
 	}
